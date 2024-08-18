1eedf744df2bd37b308d8444857c06553d3b7d39
Furzorg <furzorg@gmail.com>
2024-08-18 16:50:18 +0200

    New version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+fetch('developments/developments_cards.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('developments').innerHTML = data);
+
 $(document).ready(function(){
     $('.modal').modal();
 });

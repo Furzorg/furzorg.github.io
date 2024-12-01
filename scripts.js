f6ca45fe68a0e3c52238845b9f2fb24679950465
Furzorg <50847339+Furzorg@users.noreply.github.com>
2024-12-01 19:44:40 +0100

    Update random

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-fetch('developments/developments_cards.html')
+fetch('developments/developments_cards1.html')
     .then(response => response.text())
-    .then(data => document.getElementById('developments').innerHTML = data);
+    .then(data => document.getElementById('developments1').innerHTML = data);
+
+fetch('developments/developments_cards2.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('developments2').innerHTML = data);
+
+fetch('streams/stream1.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('stream1').innerHTML = data);
 
 $(document).ready(function(){
     $('.modal').modal();
